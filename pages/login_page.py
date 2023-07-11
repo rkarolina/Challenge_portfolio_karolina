d963901b428318dfba6c4e8efdb8ace7e11befd8
rkarolina <135992492+rkarolina@users.noreply.github.com>
2023-07-11 23:08:44 +0200

    3_subtask_1_2_

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,10 +4,18 @@ from pages.base_page import BasePage
 class LoginPage(BasePage):
     login_field_xpath = "//*[@id='login']"
     password_field_xpath = "//*[@id='password']"
-    sign_in_button_xpath = "//*[@class='MuiTouchRipple-root']"
+    sign_in_button_xpath = "//*[text()='Sign in']"
+    login_url = ('https://scouts-test.futbolkolektyw.pl/en')
+    expected_title = "Scouts panel - sign in"
 
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
+    def type_in_password(self, password):
+        self.field_send_keys(self.password_field_xpath, password)
+    def click_sign_in_button(self):
+        self.click_on_the_element(self.sign_in_button_xpath)
 
+    # def title_of_page(self):
+    #     assert
 
-breakpoint()
+# breakpoint()
---
+++ test_cases/login_to_the_system.py
@@ -0,0 +1,38 @@
+import os
+import time
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+from pages.dashboard import Dashboard
+from pages.login_page import LoginPage
+from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
+
+
+class TestLoginPage(unittest.TestCase):
+
+    driver = None
+
+    @classmethod
+    def setUp(self):
+        os.chmod(DRIVER_PATH, 755)
+        self.driver = webdriver.Chrome(service=Service(DRIVER_PATH))
+        # self.driver_service = Service(executable_path=ChromeDriverManager().install())
+        # self.driver = webdriver.Chrome(service=self.driver_service)
+        self.driver.get('https://scouts-test.futbolkolektyw.pl/en')
+        self.driver.fullscreen_window()
+        self.driver.implicitly_wait(IMPLICITLY_WAIT)
+
+    def test_login_to_the_system(self):
+        user_login = LoginPage(self.driver)
+        user_login.type_in_email('user01@getnada.com')
+        user_login.type_in_password('Test-1234')
+        user_login.click_sign_in_button()
+        # dashboard_page = Dashboard(self.driver)
+        # dashboard_page.title_of_page()
+        time.sleep(5)
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
+
