eb6e5ec392e4516b57f1dc045ead53e98861ed09
rkarolina <135992492+rkarolina@users.noreply.github.com>
2023-07-24 12:39:34 +0200

    5_0

---
+++ all_suite_test.py
@@ -0,0 +1,24 @@
+import unittest
+
+from unittest.loader import makeSuite
+
+from test_cases.add_a_new_match import TestAddNewMatch
+from test_cases.download_players_csv_list import TestDownloadCsvFile
+from test_cases.framework import Test
+from test_cases.go_to_add_a_new_player import TestAddPlayer
+from test_cases.go_to_edit_new_player import TestEditPlayer
+from test_cases.login_to_the_system import TestLoginPage
+
+
+def full_suite():
+   test_suite = unittest.TestSuite()
+   test_suite.addTest(makeSuite(TestLoginPage))
+   test_suite.addTest(makeSuite(TestDownloadCsvFile))
+   test_suite.addTest(makeSuite(TestAddPlayer))
+   test_suite.addTest(makeSuite(TestEditPlayer))
+   test_suite.addTest(makeSuite(TestAddNewMatch))
+   return test_suite
+
+if __name__ == '__main__':
+   runner = unittest.TextTestRunner(verbosity=2)
+   runner.run(full_suite())
