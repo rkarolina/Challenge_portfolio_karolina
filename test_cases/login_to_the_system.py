522314311bec71764fe762b9d6d8b72162230b19
Karolina Rozbicka <135992492+rkarolina@users.noreply.github.com>
2023-10-21 22:56:45 +0200

    Update login_to_the_system.py

--- test_cases/login_to_the_system.py
+++ test_cases/login_to_the_system.py
@@ -25,8 +25,8 @@ class TestLoginPage(unittest.TestCase):
     def test_login_to_the_system(self):
         user_login = LoginPage(self.driver)
         user_login.title_of_page()
-        user_login.type_in_email('user01@getnada.com')
-        user_login.type_in_password('Test-1234')
+        user_login.type_in_email(' ')
+        user_login.type_in_password(' ')
         user_login.click_sign_in_button()
         dashboard_page = Dashboard(self.driver)
         dashboard_page.title_of_page()
